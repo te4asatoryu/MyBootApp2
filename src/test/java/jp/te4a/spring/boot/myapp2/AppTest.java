db9622b6d70023a12b448b615286708a8dfbd01b
佐藤 隆太 <180598@jc-21.jp>
2021-05-27 13:11:31 +0900

    test_update

--- src/test/java/jp/te4a/spring/boot/myapp2/AppTest.java
+++ src/test/java/jp/te4a/spring/boot/myapp2/AppTest.java
@@ -1,5 +1,6 @@
 package jp.te4a.spring.boot.myapp2;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -17,4 +18,24 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+    public void test_hello()
+    {
+	    String greeting = "hello";
+    	HelloController hellocontroller = new HelloController();
+		String actual =  hellocontroller.index();
+	    assertEquals(greeting,actual);
+    }
+
+    @Test
+    public void test_taro()
+    {
+	    String taro = "taro deat";
+    	HelloController hellocontroller = new HelloController();
+		String actual =  hellocontroller.taro();
+	    assertEquals(taro,actual);
+    }
+    
+    
 }
